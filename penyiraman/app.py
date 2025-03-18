29e205b76738cebb109e51ab9285e1864c6cf8d6
aktiaau <aktia.au@gmail.com>
2025-03-18 21:18:55 +0700

    1803252045

--- penyiraman/app.py
+++ penyiraman/app.py
@@ -6,5 +6,9 @@ app=Flask(__name__) #membuat class atau object
 def index():
     return render_template('index.html')
 
+@app.route('/developer')
+def developer():
+    return render_template('developer.html')
+
 if __name__=='__main__':#menjalankan program
     app.run(debug=True)
